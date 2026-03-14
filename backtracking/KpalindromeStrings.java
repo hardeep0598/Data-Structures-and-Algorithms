5194da21827a1b5072fb7b4734f1c48c4101489b
Hardeep Kaur <hardeepkaur.hkdev@gmail.com>
2026-03-14 16:45:39 +0800

    Add KpalindromeStrings class with canConstruct method

---
+++ backtracking/KpalindromeStrings.java
@@ -0,0 +1,14 @@
+class KpalindromeStrings {
+
+    public boolean canConstruct(String s, int k) {
+        if (k > s.length()) return false;
+
+        int mask = 0;
+        for (char c : s.toCharArray()) {
+            mask ^= 1 << (c - 'a'); // toggle bit for character
+        }
+
+        int oddCount = Integer.bitCount(mask);
+        return oddCount <= k;
+    }
+}
