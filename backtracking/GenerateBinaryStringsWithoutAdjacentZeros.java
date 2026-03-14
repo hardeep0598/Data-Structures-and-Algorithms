78f7fdf5c82b6729ecdf99182be4582b394ecc99
Hardeep Kaur <hardeepkaur.hkdev@gmail.com>
2026-03-14 09:22:05 +0800

    Implement iterative method for binary string generation
    Added an iterative method to generate binary strings without adjacent zeros.

--- backtracking/GenerateBinaryStringsWithoutAdjacentZeros.java
+++ backtracking/GenerateBinaryStringsWithoutAdjacentZeros.java
@@ -9,9 +9,36 @@ public class GenerateBinaryStringsWithoutAdjacentZeros {
     public static void main(String[] args) {
         int n = 2;
 
+        // Call Iterative Version
+        System.out.println("Iterative result: " + generateBinaryStringsWithoutAdjacentZerosIterative(n));
+
         // Call Recursive Version
         generateBinaryStringsWithoutAdjacentZerosRecursive(n, "");
         System.out.println("Recursive result: " + result);
+    }
+
+  public static List<String> generateBinaryStringsWithoutAdjacentZerosIterative(int n) {
+        Queue<String> queue = new LinkedList<>();
+        List<String> result = new ArrayList<>();
+        queue.add("");
+
+        while (!queue.isEmpty()) {
+            String s = queue.poll();
+
+            if (s.length() == n) {
+                result.add(s);
+                continue;
+            }
+
+            // Always allowed to append '1'
+            queue.add(s + "1");
+
+            // Append '0' only if last char is not '0'
+            if (s.isEmpty() || s.charAt(s.length() - 1) != '0') {
+                queue.add(s + "0");
+            }
+        }
+        return result;
     }
 
   public static void generateBinaryStringsWithoutAdjacentZerosRecursive(int n, String prefix, char lastChar) {
