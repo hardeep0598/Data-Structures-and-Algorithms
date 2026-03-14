91069277d61168dc03238bc8c4369dbfa9664f04
Hardeep Kaur <hardeepkaur.hkdev@gmail.com>
2026-03-14 00:58:15 +0000

    feat: add backtracking module

---
+++ backtracking/GenerateBinaryStrings.java
@@ -0,0 +1,40 @@
+package backtracking;
+
+import java.util.*;
+
+public class GenerateBinaryStrings {
+
+    private static List<String> result = new ArrayList<>();
+    
+    public static void main(String[] args) {
+        int n = 2;
+
+        // Call Iterative Version
+        System.out.println("Iterative result: " + generateBinaryStringsIterative(n));
+
+        // Call Recursive Version
+        generateBinaryStringsRecursive(n, "");
+        System.out.println("Recursive result: " + result);
+    }
+    
+    private static List<String> generateBinaryStringsIterative(int n) {
+        List<String> result = new ArrayList<>();
+        for(int i=0; i<Math.pow(2, n); i++) {
+            String binary = Integer.toBinaryString(i);
+            String padded = "0".repeat(n - binary.length()) + binary;
+            result.add(padded);
+        }
+        return result;
+    }
+
+    public static void generateBinaryStringsRecursive(int n, String prefix) {
+        if (n == 0) {
+            result.add(prefix);
+            return;
+        }
+        // Append '0' and recurse
+        generateBinaryStringsRecursive(n - 1, prefix + "0");
+        // Append '1' and recurse
+        generateBinaryStringsRecursive(n - 1, prefix + "1");
+    }
+}
